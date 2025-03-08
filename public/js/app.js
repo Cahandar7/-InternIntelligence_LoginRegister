17648bdf1b16eaf444c98fd352f8083a610428c7
Cahandar7 <cahandar717@gmail.com>
2025-03-08 11:58:22 +0400

    Updated project

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-document.addEventListener("mousemove", (e) => {
-  document.documentElement.style.setProperty(
-    "--move-x",
-    `${(e.clientX - window.innerWidth / 2) * -0.005}deg`
-  );
-  document.documentElement.style.setProperty(
-    "--move-y",
-    `${(e.clientY - window.innerHeight / 2) * 0.01}deg`
-  );
+window.addEventListener("load", () => {
+  document.addEventListener("mousemove", (e) => {
+    Object.assign(document.documentElement, {
+      style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * 0.01}deg;
+      `,
+    });
+  });
 });
